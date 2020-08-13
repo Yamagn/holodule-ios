a6b46547520bfbb6193a6d0b619a25c5fb60cb95
Yamagn <syankenpon@gmail.com>
2020-08-13 17:07:33 +0900

    Modelの追加

--- holodule-ios/Models/Responses.swift
+++ holodule-ios/Models/Responses.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+public struct ChannelList: Codable {
+    let items: [ChannelItem]
+}
+public struct ChannelItem: Codable {
+    let snippet: Snippet
+}
+public struct Snippet: Codable {
+    let title: String
+    let thumbnails: Thumbnail
+}
+public struct Thumbnail: Codable {
+    let `default`: ThumbnailInfo
+    let medium: ThumbnailInfo
+    let high: ThumbnailInfo
+}
+public struct ThumbnailInfo: Codable {
+    let url: String
+    let width: Double
+    let height: Double
+}
