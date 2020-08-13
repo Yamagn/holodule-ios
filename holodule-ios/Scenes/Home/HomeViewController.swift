ed0de4438c9bcbea542155009511236a1256360a
Yamagn <syankenpon@gmail.com>
2020-08-13 09:46:26 +0900

    tableViewを追加

--- holodule-ios/Scenes/Home/HomeViewController.swift
+++ holodule-ios/Scenes/Home/HomeViewController.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2020 ymgn. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+class HomeViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let channelNib = UINib(nibName: "ChannelCell", bundle: nil)
+        tableView.register(channelNib, forCellReuseIdentifier: "ChannelCell")
+        tableView.indexPathsForSelectedRows?.forEach { [weak self] in
+            self?.tableView.deselectRow(at: $0, animated: true)
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
