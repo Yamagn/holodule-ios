8eb5669cdfe845a83855febfe0fd57ea8eb8be09
Yamagn <syankenpon@gmail.com>
2020-08-16 19:52:04 +0900

    Modelの変更と時間関係の追加

--- holodule-ios/Scenes/Main/MainViewController.swift
+++ holodule-ios/Scenes/Main/MainViewController.swift
@@ -14,7 +14,9 @@ import Kingfisher
 
 class MainViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
-    var channels: ChannelList?
+    var channels: [Channel]?
+    var videos: [Video]?
+    let dateFormatter = DateFormatter()
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -31,18 +33,55 @@ class MainViewController: UIViewController {
         Session.send(GetChannelList()) { result in
             switch result {
                 case.success(let res):
-                    self.channels = res
+                    self.channels = res.channels
                     self.tableView.reloadData()
-                case .failure(let res):
-                    print("fetch failure")
+                case .failure(let err):
+                    print(err)
             }
         }
+        IDS.forEach { id in
+            Session.send(GetVideos()) { result in
+                switch result {
+                    case .success(let res):
+                        self.videos = self.sortVideo(videos: res.videos)
+                        self.tableView.reloadData()
+                    case .failure(let err):
+                        print(err)
+                }
+            }
+        }
+    }
+    func isoStringToDate(src: String) -> Date? {
+        dateFormatter.locale = Locale(identifier: "ja_JP")
+        dateFormatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'"
+        return dateFormatter.date(from: src)
+    }
+    func isoDateToString(src: Date) -> String {
+        dateFormatter.timeStyle = .medium
+        dateFormatter.dateStyle = .medium
+        return dateFormatter.string(from: src)
+    }
+    func convertScheduledAt(video: Video) -> Date {
+        if video.isStream {
+            return isoStringToDate(src: video.scheduledStartTime!)!
+        }
+        return isoStringToDate(src: video.publishedAt)!
+    }
+    func sortVideo(videos: [Video]) -> [Video] {
+        var srcVideos = videos
+        srcVideos.sort { prevVideo, nextVideo in
+            convertScheduledAt(video: prevVideo) < convertScheduledAt(video: nextVideo)
+        }
+        return srcVideos
     }
 }
 
 extension MainViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        guard let schedulesCount = self.videos?.count else {
+            return 1
+        }
+        return schedulesCount + 1 
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         switch indexPath.row {
@@ -51,7 +90,32 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
             print(cell)
             return cell
         default:
+            let schedule = self.videos?[indexPath.row - 1]
             let cell: ScheduleCell = tableView.dequeueReusableCell(withIdentifier: "ScheduleCell") as! ScheduleCell
+            
+            let channelOptional = self.channels?.filter { $0.channelId == schedule?.channelId ?? "" }[0]
+            guard let channel = channelOptional else { return cell }
+            
+            let processer = DownsamplingImageProcessor(size: cell.channelThumbnail.bounds.size) |> RoundCornerImageProcessor(cornerRadius: 20)
+            let videoThumbnailUrl = URL(string: schedule?.thumbnailUrl ?? "")
+            let channelThumbnailUrl = URL(string: channel.thumbnailUrl)
+            
+            cell.videoThumbnail.kf.setImage(with: videoThumbnailUrl, placeholder: UIImage(named: "no_image.png"))
+            cell.channelName.text = schedule?.channelTitle
+            cell.channelThumbnail.kf.indicatorType = .activity
+            cell.channelThumbnail.kf.setImage(with: channelThumbnailUrl, placeholder: UIImage(named: "no_image.png"), options: [.processor(processer), .scaleFactor(UIScreen.main.scale), .transition(.fade(1)), .cacheOriginalImage])
+            
+            if schedule?.isStream ?? false {
+                let scheduledAtDate = isoStringToDate(src: (schedule?.scheduledStartTime!)!)!
+                let scheduledAtStr = isoDateToString(src: scheduledAtDate)
+                let sepalatedTime = scheduledAtStr.components(separatedBy: " ")[1].components(separatedBy: ":")
+                cell.scheduledAt.text = sepalatedTime[0] + ":" + sepalatedTime[1]
+            } else {
+                let publishedAtDate = isoStringToDate(src: (schedule?.publishedAt)!)!
+                let publishedAtStr = isoDateToString(src: publishedAtDate)
+                let sepalatedTime = publishedAtStr.components(separatedBy: " ")[1].components(separatedBy: ":")
+                cell.scheduledAt.text = sepalatedTime[0] + ":" + sepalatedTime[1]
+            }
             return cell
         }
     }
@@ -66,13 +130,13 @@ extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSour
         guard let channels = self.channels else {
             return 0
         }
-        return channels.items.count
+        return channels.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell: ThumbnailCell = collectionView.dequeueReusableCell(withReuseIdentifier: "ThumbnailCell", for: indexPath) as! ThumbnailCell
         guard let channels = self.channels else { return cell }
-        let url = URL(string: channels.items[indexPath.row].snippet.thumbnails.default.url)
+        let url = URL(string: channels[indexPath.row].thumbnailUrl)
         let processer = DownsamplingImageProcessor(size: cell.thumbnailView.bounds.size) |> RoundCornerImageProcessor(cornerRadius: 20)
         cell.thumbnailView.kf.indicatorType = .activity
         cell.thumbnailView.kf.setImage(with: url, placeholder: UIImage(named: "no_image.png"), options: [.processor(processer), .scaleFactor(UIScreen.main.scale), .transition(.fade(1)), .cacheOriginalImage]){
